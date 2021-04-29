7769fa3ea7ffda1508243964083f874809909b72
sergey <sergo231993@gmail.com>
2021-04-29 19:38:39 +0300

    hello

--- src/main/java/com/example/gittest/controller/rest/StudentRestController.java
+++ src/main/java/com/example/gittest/controller/rest/StudentRestController.java
@@ -1,4 +1,13 @@
 package com.example.gittest.controller.rest;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class StudentRestController {
+
+    @RequestMapping
+    String sayHello(){
+        return  "<h1> Hello </h1>";
+    }
 }
